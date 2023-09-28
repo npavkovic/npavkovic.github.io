2b13d866b6d5951a288702c1310596c62ca3dca4
Nicholas Pavkovic <nicholas@pavkovic.com>
2023-09-27 20:13:21 -0700

    Revised edge function

--- netlify/edge-functions/protect-fonts.js
+++ netlify/edge-functions/protect-fonts.js
@@ -1,20 +1,21 @@
 export default async (request, context) => {
  const restrictedDirectory = "/assets";
- const allowedHosts = ["netlify.app", "pavkovic.com", "npavkovic.github.io", "localhost"];
+ const allowedHosts = [
+  "netlify.app",
+  "pavkovic.com",
+  "npavkovic.github.io",
+  "localhost",
+ ];
  // Get the path of the requested resource
- const requestedPath = request.url.pathname;
+ const host = request.headers.host;
 
- // Check if the requested path is within the restricted directory
- if (requestedPath.startsWith(restrictedDirectory)) {
-  // Get the host of the incoming request
-  const host = request.headers.host;
-
-  // Check if the host is on the list of allowed hosts
-  if (!allowedHosts.includes(host)) {
-   // Return a 403 Forbidden response
-   return new Response("Forbidden", { status: 403 });
-  }
+ // Check if the host is on the list of allowed hosts
+ if (!allowedHosts.includes(host)) {
+  // Return a 403 Forbidden response
+  return new Response("Forbidden", { status: 403 });
  }
 
  // Otherwise, process the request as usual
 };
+
+export const config = { path: "/assets" };
