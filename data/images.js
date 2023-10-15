2d881acd2bfdfe100b37b87dcade4ffc3c34638f
Nicholas Pavkovic <nicholas@pavkovic.com>
2023-10-15 12:46:59 -0700

    October 15

--- data/images.js
+++ data/images.js
@@ -1,44 +1,93 @@
-const cloudinary = require('cloudinary').v2;
+const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-	cloud_name: 'dhenrau1q',
-	api_key: '587886426691953',
-	api_secret: 'nfEQI6Nrhs0IW0y-2P_nikwNHPM'
+	cloud_name: "dhenrau1q",
+	api_key: "587886426691953",
+	api_secret: "nfEQI6Nrhs0IW0y-2P_nikwNHPM",
 });
 
 module.exports = function () {
-
 	const theImages = {};
-	return cloudinary.api.resources({
-		tags: true,
-		context: true,
-		type: 'upload',
-		prefix: 'portfolio',
-		max_results: 500
-	})
-	.then(images => {
+	let imageCount = 0;
 
-			images.resources.forEach( image => {
-				const theTag = image.tags && image.tags.length > 0 ? image.tags[0] : 'notag';
+	return cloudinary.api
+		.resources({
+			tags: true,
+			context: true,
+			type: "upload",
+			prefix: "portfolio",
+			max_results: 500,
+		})
+		.then((images) => {
+			imageCount = 0;
+			for (let image of images.resources) {
+				const theTag = image.tags && image.tags.length > 0 ? image.tags[0] : "notag";
 				theImages[theTag] = theImages[theTag] || [];
+
+				// Grouped image?
+				if (image.context?.custom?.groupWith) {
+					// console.log("Grouped image", image.context?.custom?.groupWith, "Main?", !!image.context?.custom?.groupMain);
+					const existingGroup = theImages[theTag].find((theImage) => {
+						// console.log(theImage.groupWith, image.context?.custom?.groupWith);
+						return theImage.groupWith === image.context?.custom?.groupWith;
+					});
+					if (existingGroup) {
+						// console.log("Adding to existing group", existingGroup.title);
+						existingGroup.urls.push(cloudinary.url(image.public_id, { secure: true }));
+						// Is this the group's main image? Then it has the meta data used for the group
+						if (image.context?.custom?.groupMain) {
+							Object.assign(existingGroup, {
+								title: image.context?.custom?.caption || "",
+								description: image.context?.custom?.alt || "",
+								eyebrow: image.context?.custom?.eyebrow || "",
+								tag: image.tags && image.tags.length > 0 ? image.tags[0] : "",
+								order: parseInt(image.context?.custom?.order) || 100,
+								groupWith: image.context?.custom?.groupWith || "",
+							});
+						}
+						continue; // Skip the rest of the loop. We're done with this image.
+					}
+				}
+
+				const theUrl = cloudinary.url(image.public_id, { secure: true });
+
 				theImages[theTag].push({
-					title: image.context?.custom?.caption || '',
-					description: image.context?.custom?.alt || '',
-					eyebrow: image.context?.custom?.eyebrow || '',
-					tag: image.tags && image.tags.length > 0 ? image.tags[0] : '',
-					url: cloudinary.url(image.public_id, { secure: true }),
-					order: parseInt(image.context?.custom?.order) || 100
-				});
-			});
+     title: image.context?.custom?.caption || "",
+     description: image.context?.custom?.alt || "",
+     eyebrow: image.context?.custom?.eyebrow || "",
+     tag: image.tags && image.tags.length > 0 ? image.tags[0] : "",
+     urls: [theUrl],
+     order: parseInt(image.context?.custom?.order) || 100,
+     groupWith: image.context?.custom?.groupWith || "",
+     slug: theUrl.split("/").pop(),
+     thumbnail: `${theUrl.replace(
+      "/upload/",
+      `/upload/c_thumb,w_200/`
+     )}.jpg`,
+    });
+			}
 
 			// Sort each tag by order
-			Object.keys(theImages).forEach( tag => {
+			imageCount = 0;
+			Object.keys(theImages).forEach((tag) => {
+				console.log(`
+/****/
+${tag}
+/****/
+`);
 				theImages[tag].sort((a, b) => {
 					return a.order - b.order;
 				});
+
+				theImages[tag].forEach((figure) => {
+					// Show the image count and the final part of each image's URL (the filename)
+					console.log("Figure: ", figure);
+					console.log(
+						`${++imageCount}. ${figure.urls.map((url) => url.split("/").pop()).join(",")}`
+					);
+				});
 			});
-			console.log(theImages);
-			return theImages;
-		}) 
-		.catch(err => console.log(err));
-}
+			return { items: theImages, tags: Object.keys(theImages) };
+		})
+		.catch((err) => console.log(err));
+};
--- js/main.js
+++ js/main.js
@@ -2,186 +2,168 @@ import * as pulpMethods from "./pulpmodern.js";
 import * as orbitMethods from "./orbits.js";
 
 const methods = {
- ...pulpMethods,
- ...orbitMethods,
+	...pulpMethods,
+	...orbitMethods,
 };
 
 function $(selector) {
- const list = document.querySelectorAll(selector);
- if (list.length == 1) return list[0];
- else return list;
+	const list = document.querySelectorAll(selector);
+	if (list.length == 1) return list[0];
+	else return list;
 }
 
 function $id(selector) {
- return document.getElementById(selector);
+	return document.getElementById(selector);
 }
 
 const dom = {
- scrollWrap: $id("scroll-wrap"),
- main: $("main"),
- scroller: $id("scroller"),
- toc: $id("toc"),
- activeBreadcrumb: null,
- activePage: null,
- nextChapter: $id("next-chapter"),
- previousChapter: $id("previous-chapter"),
+	scrollWrap: $id("scroll-wrap"),
+	main: $("main"),
+	scroller: $id("scroller"),
+	toc: $id("toc"),
+	pages: $("page"),
+	activeBreadcrumb: null,
+	activePage: null,
+	nextChapter: $id("next-chapter"),
+	previousChapter: $id("previous-chapter"),
 };
 
-// dom.scrollWrap.scrollIntoView();
+
+// Full Screen API
 
 document.getElementById("full-screen-button").addEventListener("click", (e) => {
- document.documentElement.requestFullscreen();
- document.body.classList.add("fullscreen");
+	document.documentElement.requestFullscreen();
+	document.body.classList.add("fullscreen");
 });
 
 document.documentElement.addEventListener("fullscreenchange", (e) => {
- if (document.fullscreenElement == null)
-  document.body.classList.remove("fullscreen");
+	if (document.fullscreenElement == null) document.body.classList.remove("fullscreen");
 });
+
+// Hamburger Menu
 
 $id("hamburger").addEventListener("click", (e) => {
- if (document.body.classList.contains("toc-active")) {
-  document.body.classList.remove("toc-active");
- } else {
-  document.body.classList.add("toc-active");
-  dom.activeBreadcrumb?.classList.remove("active");
-  if (dom.activePage?.id) {
-   dom.activeBreadcrumb = dom.toc.querySelector(
-    `[data-id="${dom.activePage.dataset.id || dom.activePage.id}"]`
-   );
-   dom.activeBreadcrumb?.classList.add("active");
-  }
- }
+	if (document.body.classList.contains("toc-active")) {
+		document.body.classList.remove("toc-active");
+	} else {
+		document.body.classList.add("toc-active");
+		dom.activeBreadcrumb?.classList.remove("active");
+		if (dom.activePage?.id) {
+			dom.activeBreadcrumb = dom.toc.querySelector(
+				`[data-id="${dom.activePage.dataset.id || dom.activePage.id}"]`
+			);
+			dom.activeBreadcrumb?.classList.add("active");
+		}
+	}
 });
 
-// Menu Indicator
-
-// $("#menu-control").addEventListener("click", (e) => {
-// const destination = document.body.classList.contains("menu-active")
-// ? dom.scrollWrap
-// : dom.menu;
-// destination.scrollIntoView({
-// behavior: "smooth"
-// });
-// });
-
-// const menuActive = new IntersectionObserver(
-// (elements, observer) => {
-// elements.forEach((element) => {
-// document.body.classList.toggle("menu-active", element.isIntersecting);
-// });
-// },
-// {
-// root: dom.main,
-// rootMargin: "0px",
-// threshold: 0.5
-// }
-// );
-
-// menuActive.observe(dom.menu);
-
-// Slide Intersection Observer
+// Page Intersection Observer
 
 const pageEvents = {
- enter: new Event("enter"),
- exit: new Event("exit"),
+	enter: new Event("enter"),
+	exit: new Event("exit"),
 };
 
 const enterPage = new IntersectionObserver(
- (elements, observer) => {
-  elements.forEach((element) => {
-   if (element.isIntersecting) {
-    element.target.classList.add("active");
-    dom.activePage = element.target;
-    window.setTimeout(() => {
-     if (element.target.classList.contains("active")) {
-      if (element.target.id) window.location.hash = element.target.id;
-      element.target.dispatchEvent(pageEvents.enter);
-     }
-    }, 1000);
-   } else {
-    element.target.classList.remove("active");
-    element.target.dispatchEvent(pageEvents.exit);
-   }
-  });
- },
- {
-  root: dom.scroller,
-  rootMargin: "0px",
-  threshold: 0.5,
- }
+	(elements, observer) => {
+		elements.forEach((element) => {
+			if (element.isIntersecting) {
+				element.target.classList.add("active");
+				dom.activePage = element.target;
+				window.setTimeout(() => {
+					if (element.target.classList.contains("active")) {
+						if (element.target.id) window.location.hash = element.target.id;
+						element.target.dispatchEvent(pageEvents.enter);
+					}
+				}, 1000);
+			} else {
+				element.target.classList.remove("active");
+				element.target.dispatchEvent(pageEvents.exit);
+			}
+		});
+	},
+	{
+		root: dom.scroller,
+		rootMargin: "0px",
+		threshold: 0.5,
+	}
 );
 
+// This observer is used to determine which chapter (code, print, etc.) is being entered when the user scrolls vertically
+
 const enterChapter = new IntersectionObserver(
- (elements, observer) => {
-  elements.forEach((element) => {
-   if (element.isIntersecting) {
-    console.log(
-     `Intersecting Ratio for ${element.target.title}: ${element.intersectionRatio}\nTop: ${element.intersectionRect.top}\nBottom: ${element.intersectionRect.bottom}`
-    );
-    // If scrolling upward, the top of the intersecting rect is 0
-    // If scrolling downward, the top of the intersecting rect is greater than 0
-    if (element.intersectionRatio < 0.5) {
-     if (element.intersectionRect.top == 0) {
-      dom.previousChapter.innerHTML = element.target.title;
-      dom.previousChapter.classList.add("show");
-     } else {
-      dom.nextChapter.innerHTML = element.target.title;
-      dom.nextChapter.classList.add("show");
-     }
-     // element.target.classList.add('entering');
-    } else {
-     dom.previousChapter.classList.remove("show");
-     dom.nextChapter.classList.remove("show");
-    }
-    // else element.target.classList.remove('entering')
-   }
-   // } else element.target.classList.remove('entering')
-  });
- },
- {
-  root: dom.scroller,
-  rootMargin: "0px",
-  threshold: [0.1, 1],
- }
+	(elements, observer) => {
+		elements.forEach((element) => {
+			if (element.isIntersecting) {
+				console.log(
+					`Intersecting Ratio for ${element.target.title}: ${element.intersectionRatio}\nTop: ${element.intersectionRect.top}\nBottom: ${element.intersectionRect.bottom}`
+				);
+				// If scrolling upward, the top of the intersecting rect is 0
+				// If scrolling downward, the top of the intersecting rect is greater than 0
+				if (element.intersectionRatio < 0.5) {
+					if (element.intersectionRect.top == 0) {
+						dom.previousChapter.innerHTML = element.target.title;
+						dom.previousChapter.classList.add("show");
+					} else {
+						dom.nextChapter.innerHTML = element.target.title;
+						dom.nextChapter.classList.add("show");
+					}
+					// element.target.classList.add('entering');
+				} else {
+					dom.previousChapter.classList.remove("show");
+					dom.nextChapter.classList.remove("show");
+				}
+				// else element.target.classList.remove('entering')
+			}
+			// } else element.target.classList.remove('entering')
+		});
+	},
+	{
+		root: dom.scroller,
+		rootMargin: "0px",
+		threshold: [0.1, 1],
+	}
 );
 
 $("chapter").forEach((chapter) => enterChapter.observe(chapter));
 
+	
+
 // Observe Slides and create data structure for index
 
 const siteIndex = {};
-$("page").forEach((page) => {
- enterPage.observe(page);
- // If the data-index property is not present, return
- if (page.dataset.index == undefined || !page.dataset.category) return;
- siteIndex[page.dataset.category] = siteIndex[page.dataset.category] || {
-  id: "",
-  pages: [],
- };
- if (page.dataset.subcategory)
-  siteIndex[page.dataset.category].pages.push(page);
- else siteIndex[page.dataset.category].id = page.id;
+$('page').forEach((page) => {
+	enterPage.observe(page);
+	// If the data-index property is not present, return
+	if (page.dataset.index == undefined || !page.dataset.category) return;
+	siteIndex[page.dataset.category] = siteIndex[page.dataset.category] || {
+		id: "",
+		pages: [],
+	};
+	if (page.dataset.subcategory) siteIndex[page.dataset.category].pages.push(page);
+	else siteIndex[page.dataset.category].id = page.id;
 });
 
-// Build TOC
+// Build Table of Contents
+
 const tocContent = $id("toc-content");
 const categories = Object.keys(siteIndex).sort();
 let tocContentHTML = "";
 categories.forEach((category) => {
- tocContentHTML += `<li><a data-id="${siteIndex[category].id}" href="#${siteIndex[category].id}">${category}</a>`;
- if (siteIndex[category].pages.length > 0) {
-  tocContentHTML += "<ul>";
-  siteIndex[category].pages.forEach((page) => {
-   tocContentHTML += `<li><a data-id="${page.id}" href="#${page.id}">${page.dataset.subcategory}</a></li>`;
-  });
-  tocContentHTML += "</ul>";
- }
- tocContentHTML += "</li>";
+	tocContentHTML += `<li><a data-id="${siteIndex[category].id}" href="#${siteIndex[category].id}">${category}</a>`;
+	if (siteIndex[category].pages.length > 0) {
+		tocContentHTML += "<ul>";
+		siteIndex[category].pages.forEach((page) => {
+			tocContentHTML += `<li><a data-id="${page.id}" href="#${page.id}">${page.dataset.subcategory}</a></li>`;
+		});
+		tocContentHTML += "</ul>";
+	}
+	tocContentHTML += "</li>";
 });
+
 tocContent.innerHTML = tocContentHTML;
-tocContent.addEventListener("click", (e) => {
- document.body.classList.remove("toc-active");
+dom.toc.addEventListener("click", (e) => {
+	document.body.classList.remove("toc-active");
 });
 
 // Scan and assign page events
@@ -189,96 +171,53 @@ tocContent.addEventListener("click", (e) => {
 const pages = document.querySelectorAll("page");
 
 pages.forEach((page) => {
- const enterMethod = page.getAttribute("@enter");
- const exitMethod = page.getAttribute("@exit");
+	const enterMethod = page.getAttribute("@enter");
+	const exitMethod = page.getAttribute("@exit");
 
- if (enterMethod && methods[enterMethod]) {
-  page.addEventListener("enter", methods[enterMethod]);
- }
+	if (enterMethod && methods[enterMethod]) {
+		page.addEventListener("enter", methods[enterMethod]);
+	}
 
- if (exitMethod && methods[exitMethod]) {
-  page.addEventListener("exit", methods[exitMethod]);
- }
+	if (exitMethod && methods[exitMethod]) {
+		page.addEventListener("exit", methods[exitMethod]);
+	}
 });
+
+// Fade in images when they are loaded by adding the loaded class
+const images = document.querySelectorAll("figure img");
+document.addEventListener(
+	"load",
+	(e) => {
+		if (e.target.tagName !== "IMG") return;
+		e.target.classList.add("loaded");
+	},
+	true
+);
 
 // Home
 
-/* 
-	// These are not currently used — here for when we add left and right paddles
-
-	$("#next-slide").addEventListener("click", () =>
-	nextSlide($(window.location.hash))
-	);
-
-	$("#previous-slide").addEventListener("click", () =>
-	previousSlide($(window.location.hash))
-	);
-}
-
-	function nextSlide(thisSlide) {
-	const nextId = thisSlide.nextElementSibling.id;
-	if (nextId) window.location.hash = nextId;
-	else window.location.hash = "home";
-	}
-
-
-
-const lightbox = (e) => {
-	console.log("Called Lightbox")
-	const images = e.target.parentElement.querySelectorAll("img");
-	const theScrim = $id("scrim");
-
-	theScrim.innerHTML = images[0];
-	theScrim.classList.add("active");
-};
-
-
-document.querySelectorAll('button').forEach( button => {
-	console.log("Assigning butotn")
-	button.addEventListener('click', lightbox)
-})
-
-*/
-
-// Add a hashchange event listener to scroll to the element with the hash
-// window.addEventListener("hashchange", (e) => {
-// 	console.log("Hash changed", window.location.hash)
-// 	$(window.location.hash).scrollIntoView({ behavior: "smooth" });
-// });
-
 function loadStyles(filePath) {
- if (document.querySelector(`link[href="${filePath}"]`)) return;
- const link = document.createElement("link");
- link.rel = "stylesheet";
- link.href = filePath;
- document.head.appendChild(link);
+	if (document.querySelector(`link[href="${filePath}"]`)) return;
+	const link = document.createElement("link");
+	link.rel = "stylesheet";
+	link.href = filePath;
+	document.head.appendChild(link);
 }
 
-function generateRandomString(length = 32) {
- let result = "";
- const halfway = Math.floor(length / 2);
-
- for (let i = 0; i < length; i++) {
-  let randomType = Math.floor(Math.random() * 3); // Randomly decide between digit, upper, or lower case character
-
-  // if (i === halfway) result += "<headline></headline>";
-
-  switch (randomType) {
-   case 0: // digit
-    result += String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-    break;
-   case 1: // upper case letter
-    result += String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-    break;
-   case 2: // lower case letter
-    result += String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-    break;
-  }
- }
-
- return result;
+function generateRandomString(count) {
+	let result = "";
+	for (let i = 0; i < count; i++) {
+		// Generate a random number from 0 to 25
+		// Transform it into a character code from 65 to 90 (A to Z)
+		// Or from 97 to 122 (a to z)
+		// Depending on a 50/50 coin flip
+		result += String.fromCharCode(
+			Math.floor(Math.random() * 26) + (Math.random() < 0.5 ? 65 : 97)
+		);
+	}
+	return result;
 }
 
-document
- .querySelectorAll("#backdrop > div > p")
- .forEach((p) => (p.innerHTML = generateRandomString(50)));
+$id("backdrop").innerHTML = `<div>${generateRandomString(400)}</div>
+				<div>${generateRandomString(400)}</div>
+				<div>${generateRandomString(400)}</div>`;
